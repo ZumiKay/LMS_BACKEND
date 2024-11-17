558f579a92645444c02129db8fc44f65e22e03c1
Zumi Kei <zumikei@Zumis-MacBook-Pro.local>
2024-11-17 14:00:54 +0700

    Second Deploy

--- dist/config/Initial.js
+++ dist/config/Initial.js
@@ -8,10 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = __importDefault(require("./database"));
 const InitalStartSever = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        // await sequelize.sync();
+        yield database_1.default.sync();
         // await SeedAdmin();
         // await Promise.all(
         //   ["Action", "Romance", "Astrology", "Health", "Mystery"].map((cate) =>
